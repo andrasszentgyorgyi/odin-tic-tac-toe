5b31d912d3711991078b6c9e97fbcdfe8b9980d7
andrasszentgyorgyi <181062883+andrasszentgyorgyi@users.noreply.github.com>
2026-03-02 17:04:44 +0100

    Added a function to check for wins- it checks for horizontal, vertical, and diagonal lines and returns the winning player.

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,16 @@
-const gameBoard = (function (){
+const gameBoard = (function () {
     let _boardSize = 3;
     let board = []
+    let lastPlaced;
 
-    function setBoardSize(val){
+    function setBoardSize(val) {
         _boardSize = val;
     }
 
-    function createBoard(){
-        for(let i = 0; i<_boardSize; i++){
+    function createBoard() {
+        for (let i = 0; i < _boardSize; i++) {
             let row = [];
-            for(let j = 0; j<_boardSize; j++){
+            for (let j = 0; j < _boardSize; j++) {
                 row[j] = null;
             }
             board[i] = row;
@@ -17,26 +18,30 @@ const gameBoard = (function (){
         return board;
     }
 
-    function addX(i, j){
-        if(board[i][j] == null){
+    function addX(i, j) {
+        if (board[i][j] == null) {
             board[i][j] = "X";
+            lastPlaced = "X";
+            checkWin();
         } else {
             console.log("Error! That square has already been filled.");
         }
     }
 
-    function addO(i, j){
-        if(board[i][j] == null){
+    function addO(i, j) {
+        if (board[i][j] == null) {
             board[i][j] = "O";
+            lastPlaced = "O";
+            checkWin();
         } else {
             console.log("Error! That square has already been filled.");
         }
     }
 
-    function displayBoard(){
+    function displayBoard() {
         let outputString = "";
-        for(let i = 0; i<_boardSize; i++){
-            for(let j = 0; j<_boardSize; j++){
+        for (let i = 0; i < _boardSize; i++) {
+            for (let j = 0; j < _boardSize; j++) {
                 outputString += board[i][j];
             }
             outputString += (" \n ");
@@ -44,8 +49,63 @@ const gameBoard = (function (){
         return outputString;
     }
 
-    return{
-        setBoardSize, createBoard, addX, addO, displayBoard
+    function checkWin() {
+        //[(0,0), (0,1), (0,2)], [(1,0), (1,1), (1,2)], [(2,0), (2,1), (2,2)] HORIZONTAL
+        //[(0,0), (1,0), (2,0)], [(0,1), (1,1), (2,1)], [(0,2), (1,2), (2,2)] VERTICAL
+        //[(0,0), (1,1), (2,2)] , [(0,2), (1,1), (2,0)] DIAGONAL
+        if (lastPlaced != null) {
+            //HORIZONTAL
+            for (let i = 0; i < _boardSize; i++) {
+                for (let j = 0; j < _boardSize; j++) {
+                    if (board[i][j] != lastPlaced) {
+                        break;
+                    } if (j == _boardSize-1) {
+                        if (board[i][j] == lastPlaced) {
+                            console.log(lastPlaced + " win! (HORIZONTAL)");
+                            return lastPlaced;
+                        }
+                    }
+                }
+            }
+
+            //VERTICAL
+            for (let i = 0; i < _boardSize; i++) {
+                for (let j = 0; j < _boardSize; j++) {
+                    if (board[j][i] != lastPlaced) {
+                        break;
+                    } if (j == _boardSize-1) {
+                        if (board[j][i] == lastPlaced) {
+                            console.log(lastPlaced + " win! (VERTICAL)");
+                            return lastPlaced;
+                        }
+                    }
+                }
+            }
+            //DIAGONAL
+            if(board[0][0] == lastPlaced){
+                if(board[1][1] == lastPlaced){
+                    if(board[2][2] == lastPlaced){
+                        console.log(lastPlaced+ " win! (DIAGONAL)");
+                        return lastPlaced;
+                    }
+                }
+            }
+            if(board[0][2] == lastPlaced){
+                if(board[1][1] == lastPlaced){
+                    if(board[2][0]==lastPlaced){
+                        console.log(lastPlaced + " win! (DIAGONAL)");
+                        return lastPlaced;
+                    }
+                }
+            }
+
+        }
+        console.log("No winner yet");
+        return;
+    }
+
+    return {
+        setBoardSize, createBoard, addX, addO, displayBoard, checkWin
     };
 })();
 
